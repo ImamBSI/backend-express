e58bf911f9930b8acd7896b691d1b1690b8cef85
BADTze <mambrilliant541@gmail.com>
2026-05-24 13:42:08 +0700

    feat: update development and production scripts in package.json

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,9 @@ import path from "path";
 dotenv.config();
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-
 app.use(express.json());
 
 app.get("/", (_, res) => {
